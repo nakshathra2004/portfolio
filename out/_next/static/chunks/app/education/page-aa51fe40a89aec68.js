ff0372af51f83149da0d1860e67a4d2ee3707b0c
Kavitha Sudheendran <nakshathranambiar2004@gmail.com>
2025-06-05 10:57:59 +0530

    static website

---
+++ out/_next/static/chunks/app/education/page-aa51fe40a89aec68.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[651],{8191:(e,s,_)=>{Promise.resolve().then(_.t.bind(_,8494,23))},8494:()=>{}},e=>{var s=s=>e(e.s=s);e.O(0,[287,441,684,358],()=>s(8191)),_N_E=e.O()}]);
